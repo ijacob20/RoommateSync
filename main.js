62fa7cfcfd52a9b086a604b19b6a3c0b3520b784
Sergio Joya <sejoya104@gmail.com>
2024-03-20 19:04:27 -0400

    Configure main.js with express

--- main.js
+++ main.js
@@ -69,3 +69,81 @@ app.on('window-all-closed', () => {
 ipcMain.on('goToSecondScreen', () => {
   mainWindow.loadFile('second_screen.html');
 });
+
+
+//configuring app to express
+
+//require modules
+const express = require('express');
+const morgan = require('morgan');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+
+//create application
+const application = express();
+
+//configure application
+let port = 3000;
+let host = 'localhost';
+let url = 'mongodb+srv://demo:demo123@cluster0.pxbjzad.mongodb.net/4155-finalproject?retryWrites=true&w=majority&appName=Cluster0'
+application.set('view engine', 'ejs');
+
+//connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/demos', 
+                {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+.then(()=>{
+  application.listen(port, host, ()=>{
+        console.log('Server is running on port', port);
+    });
+})
+.catch(err=>console.log(err.message));
+
+//mount middlware
+application.use(express.static('public'));
+application.use(express.urlencoded({extended: true}));
+application.use(morgan('tiny'));
+application.use(methodOverride('_method'));
+
+application.use(session({
+    secret: 'dajsdklmvsdf',
+    resave: false, 
+    saveUninitialized: false,
+    cookie:{maxAge: 60*60*1000},
+    store: new MongoStore({mongoUrl: 'mongodb://localhost:27017/demos'})
+}));
+
+application.use(flash());
+
+application.use((req, res, next)=>{
+    //console.log(req.session);
+    res.locals.successMessages = req.flash('success');
+    res.locals.errorMessages = req.flash('error');
+    next();
+});
+
+//set up routes
+app.get('/', (req, res)=>{
+  res.render('index');
+});
+
+app.use('/users', userRoutes);
+
+app.use((req, res, next) => {
+  let err = new Error('The server cannot locate ' + req.url);
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next)=>{
+  console.log(err.stack);
+  if(!err.status) {
+      err.status = 500;
+      err.message = ("Internal Server Error");
+  }
+
+  res.status(err.status);
+  res.render('error', {error: err});
+});
