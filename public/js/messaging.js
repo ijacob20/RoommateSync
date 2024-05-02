49eebec5b9a5ee33bc73323ce270e23904e1b80f
Isaiah J <98429563+ijacob20@users.noreply.github.com>
2024-05-01 23:15:31 -0400

    Final Messages Push
    - Clean design
    - messages are dynamic now (stay in proper format)
    - Offline messaging
    - Realtime messaging
    - Sound notifications

--- public/js/messaging.js
+++ public/js/messaging.js
@@ -6,73 +6,78 @@ const chatMessages = document.querySelector('.chat-container')
 const messageForm = document.getElementById('message-form')
 const messageInput = document.getElementById('msg')
 const messageTone = new Audio('/message-tone.mp3')
+let url = window.location.pathname;
+
+let urlId = url.split("/messages/")[1];
+
+console.log('the id in this url is: ' + urlId);
 
 
 
 messageForm.addEventListener('submit', (e) => {
-  e.preventDefault()
-  sendMessage()
+e.preventDefault()
+sendMessage()
 })
 
 socket.on('clients-total', (data) => {
-  clientsTotal.innerText = `Total Clients: ${data}`
+clientsTotal.innerText = `Total Clients: ${data}`
 })
 
 function sendMessage() {
-  if (messageInput.value === '') return
-  const userName = document.querySelector('.username').textContent;
-  // console.log(messageInput.value)
-  const data = {
-    message: messageInput.value,
-    username: userName,
-    dateTime: new Date(),
-  }
-  socket.emit('message', data)
-  addMessageToUI(true, data)
-  messageInput.value = ''
+if (messageInput.value === '') return
+const userName = document.querySelector('.username').textContent;
+// console.log(messageInput.value)
+const data = {
+message: messageInput.value,
+receiver: urlId,
+dateTime: new Date(),
+}
+socket.emit('message', data)
+addMessageToUI(true, data)
+messageInput.value = ''
 }
 
 socket.on('chat-message', (data) => {
-  // console.log(data)
-  messageTone.play()
+// console.log(data)
+messageTone.play()
 
-  addMessageToUI(false, data)
+addMessageToUI(false, data)
 })
 
 function addMessageToUI(isOwnMessage, data) {
-  clearFeedback()
-    // const user = document.createElement('div');
-    
-    // user.classList.add('message-sender');
-    // user.textContent = data.username;
-    // div.append(user);
+clearFeedback()
+// const user = document.createElement('div');
 
-    const messageLi = document.createElement('li');
-    if (isOwnMessage) {
-      messageLi.classList.add('message-bubble');
-      messageLi.classList.add('message-right');
+// user.classList.add('message-sender');
+// user.textContent = data.username;
+// div.append(user);
 
-    } else {
-      messageLi.classList.add('message-left');
+const messageLi = document.createElement('li');
+if (isOwnMessage) {
+  messageLi.classList.add('message-bubble');
+  messageLi.classList.add('message-right');
 
-    }
-    messageLi.classList.add('message-bubble');
+} else {
+  messageLi.classList.add('message-left');
 
-  const pElement = document.createElement('p');
-  pElement.innerHTML = data.message;
-  const spanElement = document.createElement('span');
-  spanElement.textContent = data.username;
-  pElement.append(spanElement);
+}
+messageLi.classList.add('message-bubble');
 
-  messageLi.append(pElement);
+const pElement = document.createElement('p');
+pElement.innerHTML = data.message;
+const spanElement = document.createElement('span');
+spanElement.textContent = data.username;
+pElement.append(spanElement);
 
-    chatMessages.append(messageLi);
+messageLi.append(pElement);
 
-  scrollToBottom()
+chatMessages.append(messageLi);
+
+scrollToBottom()
 }
 
 function scrollToBottom() {
-  chatMessages.scrollTo(0, chatMessages.scrollHeight)
+chatMessages.scrollTo(0, chatMessages.scrollHeight)
 }
 
 
@@ -82,7 +87,7 @@ function scrollToBottom() {
 
 
 function clearFeedback() {
-  document.querySelectorAll('li.message-feedback').forEach((element) => {
-    element.parentNode.removeChild(element)
-  })
+document.querySelectorAll('li.message-feedback').forEach((element) => {
+element.parentNode.removeChild(element)
+})
 }
